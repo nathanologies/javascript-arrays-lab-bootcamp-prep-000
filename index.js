60ae5049fbe67868fa88339212acab4adba4d75d
Nathan Lin <nlin@theticker.org>
2018-07-16 20:00:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function appendKitten(name) {
 }
 
 function prependKitten(name) {
-  var newKittens = [kittens...]
+  var newKittens = [...kittens]
   newKittens.unshift(name)
   return newKittens
 }
